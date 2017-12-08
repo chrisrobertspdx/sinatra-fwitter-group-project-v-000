59a1bbc8fdf8d5725284b9f807e73f2387bf6ee7
Chris Roberts <chrisrobertspdx@gmail.com>
2017-12-08 19:03:01 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -32,4 +32,42 @@ class TweetsController < ApplicationController
     erb :'/tweets/create_tweet'
   end
 
+  get '/tweets/:id' do
+    #check login
+    if !logged_in?
+      redirect to "/login"
+    end
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+    if !logged_in?
+      redirect to "/login"
+    end
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/edit_tweet'
+  end
+
+  post '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(content: params[:content])
+    if @tweet.save
+      redirect to "/tweets"
+    else
+      redirect to "/tweets/#{params[:id]}/edit"
+    end
+  end
+
+  post '/tweets/:id/delete' do
+    @tweet = Tweet.find(params[:id])
+    if @tweet.user == current_user
+      @tweet.destroy
+      redirect to "/tweets"
+    else
+      redirect to "/tweets"
+    end
+
+  end
+
 end
